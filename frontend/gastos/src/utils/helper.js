bdb72a12f41c604ba3c5eb1390c0290ca2a650de
Renso Guilalas <rensoguilalas@gmail.com>
2025-07-15 15:44:20 +0100

    Create Dashboard page layout.

--- frontend/gastos/src/utils/helper.js
+++ frontend/gastos/src/utils/helper.js
@@ -2,3 +2,10 @@ export const validateEmail = (email) => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 };
+
+export const getInitials = (fullName) => {
+  return fullName
+    .split(" ")
+    .map((name) => name.charAt(0).toUpperCase())
+    .join("");
+};
