9cb55940689ca5e56c826d26c23dc3ef2a17c54b
Renso Guilalas <rensoguilalas@gmail.com>
2025-07-21 01:15:31 +0100

    Add bar chart for income, add 'add income' button, add modal.

---
+++ frontend/gastos/src/components/Income/IncomeOverview.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { LuPlus } from "react-icons/lu";
+import CustomBarChart from "../Charts/CustomBarChart";
+import { prepareIncomeBarChartData } from "../../utils/helper";
+
+const IncomeOverview = ({ transactions, onAddIncome }) => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    if (transactions && transactions.length > 0) {
+      const result = prepareIncomeBarChartData(transactions);
+      setChartData(result);
+    } else {
+      setChartData([]);
+    }
+  }, [transactions]);
+
+  return (
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <div className="">
+          <h5 className="text-lg">Income Overview</h5>
+          <p className="text-xs text-gray-400 mt-0.5">
+            Track your earning over time and analyse your income trends.
+          </p>
+        </div>
+
+        <button className="add-btn" onClick={onAddIncome}>
+          <LuPlus className="text-lg" />
+          Add Income
+        </button>
+      </div>
+
+      <div className="mt-10"></div>
+      {chartData.length > 0 ? (
+        <CustomBarChart data={chartData} />
+      ) : (
+        <div className="text-center text-gray-500 py-8">
+          No income data available
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default IncomeOverview;
--- frontend/gastos/src/pages/Dashboard/Income.jsx
+++ frontend/gastos/src/pages/Dashboard/Income.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import IncomeOverview from "../../components/Income/IncomeOverview";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { API_PATHS } from "../../utils/apiPaths";
+import axiosInstance from "../../utils/axiosInstance";
+import Modal from "../../components/Modal";
 
 const Income = () => {
-  return <div>Income</div>;
+  const [incomeData, setIncomeData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeletAlert] = useState({
+    show: false,
+    data: null,
+  });
+  const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
+
+  // Get all Income details.
+  const fetchIncomeDetails = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.INCOME.GET_ALL_INCOMES}`
+      );
+
+      if (response.data) {
+        console.log("Income data fetched:", response.data);
+        setIncomeData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong, please try again later.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Handle 'Add Income'.
+  const handleAddIncome = async (income) => {};
+
+  // Delete Income.
+  const deleteIncome = async (id) => {};
+
+  // Handle download income details.
+  const handleDownloadincomeDetails = async () => {};
+
+  useEffect(() => {
+    fetchIncomeDetails();
+  }, []);
+
+  return (
+    <DashboardLayout activeMenu="Income">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div className="">
+            {loading ? (
+              <div className="card">
+                <div className="text-center py-8">
+                  <div className="text-gray-500">Loading income data...</div>
+                </div>
+              </div>
+            ) : (
+              <IncomeOverview
+                transactions={incomeData}
+                onAddIncome={() => setOpenAddIncomeModal(true)}
+              />
+            )}
+          </div>
+        </div>
+
+        <Modal
+          isOpen={openAddIncomeModal}
+          onClose={() => setOpenAddIncomeModal(false)}
+          title="Add Income"
+        >
+          <div>Add Income Form</div>
+        </Modal>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Income;
