80dcbb2bf7dc29ea4c77a4560a96fb2b5a748815
Renso Guilalas <rensoguilalas@gmail.com>
2025-07-21 15:35:56 +0100

    Hook up API call to fetch all expenses.

---
+++ frontend/gastos/src/components/Expense/ExpenseOverview.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseOverview = ({ transactions, onExpenseIncome }) => {
+  return <div>ExpenseOverview</div>;
+};
+
+export default ExpenseOverview;
--- frontend/gastos/src/pages/Dashboard/Expense.jsx
+++ frontend/gastos/src/pages/Dashboard/Expense.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { API_PATHS } from "../../utils/apiPaths";
+import toast from "react-hot-toast";
+import axiosInstance from "../../utils/axiosInstance";
+import ExpenseOverview from "../../components/Expense/ExpenseOverview";
 
 const Expense = () => {
-  return <div>Expense</div>;
+  useUserAuth();
+
+  const [expenseData, setExpenseData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+  });
+  const [openAddExpenseModal, setOpenAddExpenseModal] = useState(false);
+
+  // Get all Expense details.
+  const fetchExpenseDetails = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.EXPENSE.GET_ALL_EXPENSES}`
+      );
+
+      if (response.data) {
+        setExpenseData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong, please try again later.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Handle 'Add Expense'.
+  const handleAddExpense = async (expense) => {
+    const { category, amount, date, icon } = income;
+
+    // Validation checks
+    if (!category.trim()) {
+      toast.error("Category is required.");
+      return;
+    }
+
+    if (!amount || isNaN(amount) || Number(amount) <= 0) {
+      toast.error("Amount should be a valid number greater than 0.");
+      return;
+    }
+
+    if (!date) {
+      toast.error("Date is required.");
+      return;
+    }
+
+    try {
+      axiosInstance.post(API_PATHS.EXPENSE.ADD_EXPENSE, {
+        category,
+        amount,
+        date,
+        icon,
+      });
+
+      setOpenAddExpenseModal(false);
+      toast.success("Expense added successfully.");
+      fetchExpenseDetails();
+    } catch (error) {
+      console.log(
+        "Error adding expense:",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
+  useEffect(() => {
+    fetchExpenseDetails();
+  }, []);
+
+  return (
+    <DashboardLayout activeMenu="Expense">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div className="">
+            <ExpenseOverview
+              transactions={expenseData}
+              onExpenseIncome={() => setOpenAddExpenseModal(true)}
+            />
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Expense;
